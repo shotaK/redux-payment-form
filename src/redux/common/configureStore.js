d057be787e0c7311f95f9d3facc77934376c8f9e
Shota Karkashadze <sh.karkashadze@gmail.com>
2017-09-23 19:32:36 +0400

    Disable redux dev tools to avoid breaking the app on non-Chrome browsers

--- src/redux/common/configureStore.js
+++ src/redux/common/configureStore.js
@@ -4,6 +4,6 @@ import {createStore} from 'redux'
 export default function configureStore() {
   return createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Uncomment if you want to use the dev tools
   );
 }

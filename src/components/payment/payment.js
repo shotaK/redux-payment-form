dbc9b31f0d87eb7039da227372f30b6072215fdd
Shota Karkashadze <sh.karkashadze@gmail.com>
2017-09-26 19:58:57 +0400

    Remove the container elements from the form

--- src/components/payment/payment.js
+++ src/components/payment/payment.js
@@ -7,15 +7,7 @@ import PaymentForm from './payment-form';
 class Payment extends Component {
   render() {
     return (
-      <div className="container payment">
-        <div className="row">
-          <div className="col-lg-6 col-lg-offset-3">
-            <div className="well bs-component">
-              <PaymentForm />
-            </div>
-          </div>
-        </div>
-      </div>
+      <PaymentForm />
     );
   }
 }
